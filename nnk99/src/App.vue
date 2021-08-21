60cda0d9e17ad0ccf45ba834ebff680dd0fcef99
v.leon.dell <trytest@email.com>
2021-08-22 01:51:15 +0300

    regular commit

--- nnk99/src/App.vue
+++ nnk99/src/App.vue
@@ -1,6 +1,16 @@
 <template>
-
+<h1>ddd</h1>
 </template>
+
+<script>
+import app from './main'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
 
 <style>
 
---
+++ nnk99/src/main.js
@@ -0,0 +1,7 @@
+const app = vue.create('app', {
+
+})
+
+module.export({
+    name: "app"
+})
